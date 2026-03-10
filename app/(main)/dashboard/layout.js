fdf1663bc5c4b7a233cf1ec1482ced45b9d677db
nisar <mominnisar190@gmail.com>
2026-03-11 00:43:51 +0530

    dashboard loading screen

--- app/(main)/dashboard/layout.js
+++ app/(main)/dashboard/layout.js
@@ -5,13 +5,13 @@ import { Suspense } from "react";
 export default function Layout() {
   return (
     <div className="px-5">
-      <div className="flex items-center justify-between mb-6">
+      <div className="flex items-center justify-between mb-4">
         <h1 className="text-6xl font-bold tracking-tight gradient-title">
           Dashboard
         </h1>
       </div>
       <Suspense
-        fallback={<BarLoader className="mt-4" width={"100%"} color="#9333ea" />}
+        fallback={<BarLoader className="mb-4" width={"100%"} color="#9333ea" />}
       >
         <DashboardPage />
       </Suspense>
